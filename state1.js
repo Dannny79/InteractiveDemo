ecc5cee76df06eebbafe1767bbf90a89ed60a946
Danny <danny_lam@hotmail.com>
2021-02-12 02:15:09 -0600

    'secondCommit'

--- state1.js
+++ state1.js
@@ -3,8 +3,14 @@ demo.state1 = function(){};
 demo.state1.prototype = {
     preload: function(){},
     create: function(){
-        game.stage.backgroundColor = '#DDDDDD';
+        game.stage.backgroundColor = '#DDDDDD'
         console.log('state1');
+        enterKey = game.input.keyboard.addKey(13)
     },
-    update: function(){}
+    update: function(){
+        if (enterKey.isDown){
+            game.state.start('state2')
+        }
+    }
 };
+
